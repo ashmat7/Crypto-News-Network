27c3a58247240f737aab5725de1226675c844830
ASHIESH MATHEWS <ashieshmathews@gmail.com>
2019-09-01 19:31:14 +0530

    More api calls added

--- crypto/views.py
+++ crypto/views.py
@@ -2,9 +2,14 @@ from django.shortcuts import render
 def home(request):
     import requests
     import json
+    #grab crypto price data
+    price_request=requests.get("https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC,XRP,ETH,BCH,EOS,LTC,XLM,ADA,USDT,MIOTA,TRX&tsyms=USD,EUR")
+    price=json.loads(price_request.content)
+
+    #grab crypto news here
     api_request=requests.get("https://min-api.cryptocompare.com/data/v2/news/?lang=EN")
     api=json.loads(api_request.content)
     for i in api['Data']:
         if(len(i['body'])>25):
             i['body']=i['body'][:35]+"..."
-    return render(request,'home.html',{'api':api})
+    return render(request,'home.html',{'api':api,'price':price})
