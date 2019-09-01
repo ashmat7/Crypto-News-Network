a677ee900182f0b34658003a239e8fb3b2b2e685
ASHIESH MATHEWS <ashieshmathews@gmail.com>
2019-09-01 13:17:12 +0530

    Limited card body text and modified appearance

--- crypto/views.py
+++ crypto/views.py
@@ -4,4 +4,7 @@ def home(request):
     import json
     api_request=requests.get("https://min-api.cryptocompare.com/data/v2/news/?lang=EN")
     api=json.loads(api_request.content)
+    for i in api['Data']:
+        if(len(i['body'])>25):
+            i['body']=i['body'][:35]+"..."
     return render(request,'home.html',{'api':api})
